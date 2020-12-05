9d6ca503cd4f82aeaeebafe0e52ba18096ccba77
Judy Tuan <judytuna@gmail.com>
2020-12-04 17:56:43 -0800

    Use memory, not sqlite.

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 "use strict";
 
 /*
-    This file will work as a standalone pub on glitch.com.
+    This file will work as a standalone pub on heroku.com, but all data will be lost at least every 24 hours.
     It will import the rest of the earthstar-pub package from npm.
+
+    Since the data is so ephemeral, this is mostly for testing purposes.
 */
 
 const pub = require('earthstar-pub');
@@ -13,14 +15,14 @@ pub.serve({
     readonly: false,  // if true, don't accept any new data from users to any workspace
     allowPushToNewWorkspaces: true,  // if true, let users add new workspaces
     discoverableWorkspaces: false,  // if true, show workspace addesses in the web interface
-    storageType: 'sqlite',  // use sqlite instead of memory to store data
-    dataFolder: '.data',  // put sqlite files here
+    storageType: 'memory',  // use memory to store data because heroku doesn't support sqlite. this will be extra-ephemeral
+    // dataFolder: '.data',  // put sqlite files here
     logLevel: 2,  // 2 = verbose, 3 = include sensitive info (workspace addresses)
 });
 
 console.log(`earthstar-pub is running on port ${port}.  ${new Date()}`);
 
-
+/*
 let printSqliteFiles = () => {
     console.log();
     let fs = require('fs');
@@ -29,3 +31,4 @@ let printSqliteFiles = () => {
     console.log();
 }
 //printSqliteFiles();
+*/
